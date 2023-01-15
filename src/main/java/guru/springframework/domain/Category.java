1cd0cc0e22c61f00ab0826265bb1b52b74cf1c2d
4kin.nikita <4kin.nikita@gmail.com>
2023-01-16 00:49:47 +0300

    10. Many To Many JPA Relationships.mp4

---
+++ src/main/java/guru/springframework/domain/Category.java
@@ -0,0 +1,41 @@
+package guru.springframework.domain;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String description;
+
+    @ManyToMany(mappedBy = "categories") // связь с переменной categories в классе Recipe
+    private Set<Recipe> recipes;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
