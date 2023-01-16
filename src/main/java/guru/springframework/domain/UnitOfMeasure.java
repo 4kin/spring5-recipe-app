58e6f2f88970679756b0d7313a622b002ae61e24
4kin.nikita <4kin.nikita@gmail.com>
2023-01-16 09:03:29 +0300

    13. Spring Data JPA Query Methods.mp4

--- src/main/java/guru/springframework/domain/UnitOfMeasure.java
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -9,7 +9,7 @@ import jakarta.persistence.*;
  */
 @Entity
 public class UnitOfMeasure {
-    @Id
+    @Id 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
