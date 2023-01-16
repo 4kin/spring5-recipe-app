69b8d7bcde0a68eed303e78870996317c99d20f9
4kin.nikita <4kin.nikita@gmail.com>
2023-01-16 09:03:29 +0300

    13. Spring Data JPA Query Methods.mp4

---
+++ src/main/java/guru/springframework/domain/test.java
@@ -0,0 +1,7 @@
+package guru.springframework.domain;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.printf(String.valueOf("category".equals("category")));
+    }
+}
