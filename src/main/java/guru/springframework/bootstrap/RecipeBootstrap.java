073e2513558ed77bb88d4323c83cdbc1e85b5f6b
4kin.nikita <4kin.nikita@gmail.com>
2023-01-22 00:36:15 +0300

    7. Creating a Command Object and Type Conversions in Spring.mp4

--- src/main/java/guru/springframework/bootstrap/RecipeBootstrap.java
+++ src/main/java/guru/springframework/bootstrap/RecipeBootstrap.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -32,9 +33,11 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
     }
 
     @Override
+    @Transactional
     public void onApplicationEvent(ContextRefreshedEvent event) {
         log.debug("Login");
         recipeRepository.saveAll(getRecipes());
+        log.debug("Loading Bootstrap Data");
     }
 
     private List<Recipe> getRecipes() {
@@ -131,9 +134,7 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
                 "Read more: http://www.simplyrecipes.com/recipes/perfect_guacamole/#ixzz4jvoun5ws");
 
 
-        guacRecipe.setUrl("строка со ссылкой");
-        guacRecipe.setServings(4);
-        guacRecipe.setSource("Ресурс который нужно добавить!");
+
 
         guacRecipe.setNotes(guacNotes);
 
@@ -149,6 +150,10 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
 
         guacRecipe.getCategories().add(americanCategory);
         guacRecipe.getCategories().add(mexicanCategory);
+
+        guacRecipe.setUrl("http://www.simplyrecipes.com/recipes/perfect_guacamole/");
+        guacRecipe.setServings(4);
+        guacRecipe.setSource("Simply Recipes");
 
         //add to return list
         recipes.add(guacRecipe);
@@ -207,6 +212,10 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
 
         tacosRecipe.getCategories().add(americanCategory);
         tacosRecipe.getCategories().add(mexicanCategory);
+
+        tacosRecipe.setUrl("http://www.simplyrecipes.com/recipes/spicy_grilled_chicken_tacos/");
+        tacosRecipe.setServings(4);
+        tacosRecipe.setSource("Simply Recipes");
 
         recipes.add(tacosRecipe);
         return recipes;
---
+++ src/test/java/guru/springframework/converters/UnitOfMeasureToUnitOfMeasureCommandTest.java
@@ -0,0 +1,48 @@
+package guru.springframework.converters;
+
+import guru.springframework.commands.UnitOfMeasureCommand;
+import guru.springframework.domain.UnitOfMeasure;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by jt on 6/21/17.
+ */
+public class UnitOfMeasureToUnitOfMeasureCommandTest {
+
+    public static final String DESCRIPTION = "description";
+    public static final Long LONG_VALUE = new Long(1L);
+
+    UnitOfMeasureToUnitOfMeasureCommand converter;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        converter = new UnitOfMeasureToUnitOfMeasureCommand();
+    }
+
+    @Test
+    public void testNullObjectConvert() throws Exception {
+        Assertions.assertNull(converter.convert(null));
+    }
+
+    @Test
+    public void testEmptyObj() throws Exception {
+        Assertions.assertNotNull(converter.convert(new UnitOfMeasure()));
+    }
+
+    @Test
+    public void convert() throws Exception {
+        //given
+        UnitOfMeasure uom = new UnitOfMeasure();
+        uom.setId(LONG_VALUE);
+        uom.setDescription(DESCRIPTION);
+        //when
+        UnitOfMeasureCommand uomc = converter.convert(uom);
+
+        //then
+        Assertions.assertNotNull(LONG_VALUE, String.valueOf(uomc.getId()));
+        Assertions.assertNotNull(DESCRIPTION, uomc.getDescription());
+    }
+
+}
