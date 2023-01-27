04b6093b3d7bf1e5055f23dea053170c8aec02cf
4kin-smp <4kin.nikita+smp@gmaill.com>
2023-01-27 17:05:37 +0300

    13. Update Ingredient.mp4 не доконца

--- src/main/java/guru/springframework/services/IngredientService.java
+++ src/main/java/guru/springframework/services/IngredientService.java
@@ -1,6 +1,7 @@
 package guru.springframework.services;
 
 import guru.springframework.commands.IngredientCommand;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by jt on 6/27/17.
@@ -8,4 +9,7 @@ import guru.springframework.commands.IngredientCommand;
 public interface IngredientService {
 
     IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
+
+    @Transactional
+    IngredientCommand saveIngredientCommand(IngredientCommand command);
 }
